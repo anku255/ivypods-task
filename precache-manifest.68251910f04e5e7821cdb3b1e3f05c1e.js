4d71507fb5d1240f3beaf649962777c2bf69fed5
Ankit Tiwari <ankitt255@gmail.com>
2019-10-23 16:07:55 +0530

    Updates

---
+++ precache-manifest.68251910f04e5e7821cdb3b1e3f05c1e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5847a78efa910a754773ec5eb9651f31",
+    "url": "/ivypods-task/index.html"
+  },
+  {
+    "revision": "f27186d86b384d679fde",
+    "url": "/ivypods-task/static/js/2.6b053766.chunk.js"
+  },
+  {
+    "revision": "e86e0e9a4a893d7158b8",
+    "url": "/ivypods-task/static/js/main.0afe057e.chunk.js"
+  },
+  {
+    "revision": "72da9ba165198343c0f1",
+    "url": "/ivypods-task/static/js/runtime-main.a8a6077a.js"
+  }
+]);
